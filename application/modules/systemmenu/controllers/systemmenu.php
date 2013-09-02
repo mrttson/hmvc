3c45fc30b41492659dad2442d82d5114804b1d14
Trinh Tien Son <son.trt9x@gmail.com>
2013-09-02 22:58:07 +0700

    fix

--- application/modules/systemmenu/controllers/systemmenu.php
+++ application/modules/systemmenu/controllers/systemmenu.php
@@ -8,14 +8,13 @@ class Systemmenu extends CommonController {
 
     public function __construct() {
         parent::__construct();
-        $this->checkPermisson();
-        $this->contentData['moduleTitle'] = 'Bộ Sưu Tập';
+        $this->contentData['moduleTitle'] = 'System Menu';
         $this->_layout = 'admin';
     }
 
     function index() {
         $this->_data['pageTitle'] = 'ADMIN';
-        $this->_data['page'] = 'admin_view';
+        $this->_data['page'] = 'index';
         $this->contentData['data'] = 'ADMIN PAGE';
         $this->_data['content'] = $this->contentData;
         $this->loadPage();
