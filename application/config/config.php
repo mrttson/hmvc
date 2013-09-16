e7890880b8c1331bf4da602fbbae96f1c383583e
Trịnh Tiến Sơn <son.trt9x@gmail.com>
2013-09-16 11:34:46 +0700

    hostinger

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/hmvc/';
+$config['base_url']	= 'http://sontrt.tk/hmvc/';
 
 /*
 |--------------------------------------------------------------------------
